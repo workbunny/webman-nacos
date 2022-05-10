9b5272f13913eae2b248d2d1d6d28c786af79fa1
chaz6chez <250220719@qq.com>
2022-05-10 23:17:23 +0800

    更新兼容程序

---
+++ src/compatible.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+use Webman\Config;
+
+if(
+    function_exists('runkit7_function_remove') and
+    PHP_MAJOR_VERSION === 7
+){
+    if(function_exists('config')){
+        runkit7_function_remove('config');
+    }
+    function config($key = null, $default = null)
+    {
+        if(!file_exists(base_path() . '/config.reload.lock')){
+            file_put_contents(base_path() . '/config.reload.lock', 1, LOCK_EX);
+            Config::reload(config_path(), ['route', 'container']);
+        }
+        return Config::get($key, $default);
+    }
+}
+else{
+    include_once './helpers.php';
+}
