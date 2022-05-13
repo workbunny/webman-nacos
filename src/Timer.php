a2a7338913ba83cb4834e0e12b95554e3eecccae
chaz6chez <250220719@qq.com>
2022-05-13 14:23:26 +0800

    添加定时器辅助

--- src/Timer.php
+++ src/Timer.php
@@ -7,10 +7,7 @@ use Workerman\Timer as WorkermanTimer;
 
 final class Timer {
 
-    /** @var int id */
-    protected static int $_id = 1;
-
-    /** @var array[] ids */
+    /** @var array[] 子定时器 */
     protected static array $_timers = [];
 
     /**
@@ -19,40 +16,30 @@ final class Timer {
      * @param float $repeat
      * @param callable $callback
      * @param ...$args
-     * @return int
+     * @return int|bool
      */
-    public static function add(float $delay, float $repeat, callable $callback, ... $args): int
+    public static function add(float $delay, float $repeat, callable $callback, ... $args)
     {
         switch (true){
             # 立即循环
             case ($delay === 0.0 and $repeat !== 0.0):
                 $callback(...$args);
-                self::$_timers[self::$_id] = [
-                    WorkermanTimer::add($repeat, $callback, $args, true)
-                ];
-                return self::$_id ++;
+                return WorkermanTimer::add($repeat, $callback, $args);
 
             # 延迟执行一次
             case ($delay !== 0.0 and $repeat === 0.0):
-                self::$_timers[self::$_id] = [
-                    WorkermanTimer::add($delay, $callback, $args, false)
-                ];
-                return self::$_id ++;
+                return WorkermanTimer::add($delay, $callback, $args, false);
 
             # 延迟循环执行，延迟与重复相同
             case ($delay !== 0.0 and $repeat !== 0.0 and $repeat === $delay):
-                self::$_timers[self::$_id] = [
-                    WorkermanTimer::add($delay, $callback, $args, true)
-                ];
-                return self::$_id ++;
+                return WorkermanTimer::add($delay, $callback, $args);
 
             # 延迟循环执行，延迟与重复不同
             case ($delay !== 0.0 and $repeat !== 0.0 and $repeat !== $delay):
-                self::$_timers[self::$_id] = [
-                    WorkermanTimer::add($delay, $callback, $args, false),
-                    WorkermanTimer::add($repeat + $delay, $callback, $args, true)
-                ];
-                return self::$_id ++;
+                return $id = WorkermanTimer::add($delay, function(...$args) use(&$id, $repeat, $callback){
+                    $callback(...$args);
+                    self::$_timers[$id] = WorkermanTimer::add($repeat, $callback, $args);
+                }, $args, false);
 
             # 立即执行
             default:
@@ -68,12 +55,22 @@ final class Timer {
      */
     public static function del(int $id): void
     {
-        if($id !== 0 and isset(self::$_timers[$id])){
-            foreach (self::$_timers as $timerId){
-                if(is_int($timerId)){
-                    WorkermanTimer::del($timerId);
-                }
-            }
+        if(
+            $id !== 0 and
+            isset(self::$_timers[$id]) and
+            is_int($timerId = self::$_timers[$id])
+        ){
+            unset(self::$_timers[$id]);
+            WorkermanTimer::del($timerId);
         }
+    }
+
+    /**
+     * @return void
+     */
+    public static function delAll(): void
+    {
+        self::$_timers = [];
+        WorkermanTimer::delAll();
     }
 }
