294a3523582cf1fc8791320471ea173c2b91597b
chaz6chez <250220719@qq.com>
2022-08-22 00:52:25 +0800

    updated: 1. 更新注释

--- src/helpers.php
+++ src/helpers.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ * This file is part of workbunny.
+ *
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @author    chaz6chez<250220719@qq.com>
+ * @copyright chaz6chez<250220719@qq.com>
+ * @link      https://github.com/workbunny/webman-nacos
+ * @license   https://github.com/workbunny/webman-nacos/blob/main/LICENSE
+ */
 declare(strict_types=1);
 
 namespace Workbunny\WebmanNacos;
